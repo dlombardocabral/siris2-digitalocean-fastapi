05464ed5309d1e83ca59b73340e072c6902d7a58
Charo <charo@Charos-MacBook-Pro.local>
2024-02-15 18:09:33 -0300

    path list de producto

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from starlette.responses import FileResponse
 from fastapi.encoders import jsonable_encoder
 import os
 import subprocess
-from monitoreo.utils import urlToGeojson
+from librerias import urlToGeojson
 
 
 app = FastAPI()
@@ -15,12 +15,10 @@ async def read_root():
 
 ## Ejecuta todas las funciones del run y genera las imagenes y el JSON con estadisticas
 @app.get('/monitoreo/{fecha}/{idLote}')
-
-def read_item(fecha:str, idLote:int):
+def monitoreo(fecha:str, idLote:int):
     try:
         #descargo del endopoint el geojson de la parcela de siris
         jsonlote=urlToGeojson(idLote)
-
     except:
         return jsonable_encoder({f'Error': 'No se pudo procesar correctamente el geoJSON de la parcela {jsonlote}'})
     pathToLote = "db/lotes/" + idLote + ".geojson"
@@ -34,3 +32,20 @@ def read_item(fecha:str, idLote:int):
     subprocess.run(["python", "run.py", fecha, pathToLote])
     return jsonable_encoder({'status': 'Imagenes generadas correctamente'})
 
+
+## Genera todas las imagenes en cascada e imprime un JSON con el status y la lista de imagenes generadas
+@app.get('/historico/{fechaInicial}/{fechaFinal}/{idLote}')
+def historico(fechaInicial:str, fechaFinal:str, idLote:int):
+    try:
+        urlToGeojson(idLote)
+    except:
+        return jsonable_encoder({'Error': 'No se pudo procesar correctamente el geoJSON de la parcela'})        
+    pathToLote = "db/lotes/" + idLote + ".geojson"
+    # Verifica si las carpetas existen, si no existen, las crea
+    productos = ["ndvi-tif", "productividad-tif", "ndvi-png", "ndvi-png-recortado", "productividad-png"]
+    for prod in productos:
+            if not os.path.exists(f"assets/{idLote}/{prod}"):
+                os.makedirs(f"assets/{idLote}/{prod}")
+    # Ejecuta los procesos
+    subprocess.run(["python", "run.py", fechaInicial, fechaFinal, pathToLote])
+    return jsonable_encoder({'status': "Imagenes generadas correctamente"})
